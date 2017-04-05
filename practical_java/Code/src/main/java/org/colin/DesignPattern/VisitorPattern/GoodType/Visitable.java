2ac6ce7f7223132549fdc9d30e6ecd9666976e16
colingood026 <colingood0@gmail.com>
2017-04-05 20:53:23 +0800

    拿掉designPattern

--- practical_java/Code/src/main/java/org/colin/DesignPattern/VisitorPattern/GoodType/Visitable.java
+++
@@ -1,8 +0,0 @@
-package org.colin.DesignPattern.VisitorPattern.GoodType;
-
-/**
- * Created by Colin on 2017/4/5.
- */
-public interface Visitable {
-    void accept(Visitor visitor);
-}
--- practical_java/Code/src/main/java/org/colin/DesignPattern/VisitorPattern/GoodType/Visitor.java
+++
@@ -1,9 +0,0 @@
-package org.colin.DesignPattern.VisitorPattern.GoodType;
-
-/**
- * Created by Colin on 2017/4/5.
- */
-public interface Visitor {
-    void visit(Member member);
-    void visit(VIP vip);
-}
